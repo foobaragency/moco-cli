f0bad8bd43e162e870738325a4caf5174217e98a
Patrick Maloney <patrick@foobar.agency>
2024-01-19 14:28:56 +0100

    feat: use lipgloss table for ls commands

--- cmd/ls.go
+++ cmd/ls.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -8,6 +5,8 @@ import (
 	"moco/data"
 	"time"
 
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 	"github.com/spf13/cobra"
 )
 
@@ -16,9 +15,13 @@ var projectCmd = &cobra.Command{
     Short: "List projects",
     Run: func(cmd *cobra.Command, args []string) {
         projects, _ := data.GetProjects()
+        t := table.New().
+            Border(lipgloss.NormalBorder()).
+            Headers("ID", "Name")
         for _, project := range projects {
-            fmt.Printf("%d %s\n", project.Id, project.Name)
+            t.Row(fmt.Sprintf("%d", project.Id), project.GetName())
         }
+        fmt.Println(t)
     },
 }
 
@@ -28,6 +31,9 @@ var taskCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         projectId, err := cmd.Flags().GetInt("project")
 
+        t := table.New().
+            Border(lipgloss.NormalBorder()).
+            Headers("ID", "Name")
         if projectId != 0 && err == nil {
             project, err := data.GetProject(projectId)
             if err != nil {
@@ -35,8 +41,9 @@ var taskCmd = &cobra.Command{
                 return
             }
             for _, task := range project.Tasks {
-                fmt.Printf("%d %s\n", task.Id, task.Name)
+                t.Row(fmt.Sprintf("%d", task.Id), task.Name)
             }
+            fmt.Println(t)
             return
         }
 
@@ -44,9 +51,10 @@ var taskCmd = &cobra.Command{
         for _, project := range projects {
             tasks := project.Tasks
             for _, task := range tasks {
-                fmt.Printf("%d %s\n", task.Id, task.Name)
+                t.Row(fmt.Sprintf("%d", task.Id), task.Name)
             }
         }
+        fmt.Println(t)
     },
 }
 
@@ -56,6 +64,10 @@ var activityCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         activities, _ := data.GetActivities()
         today, _ := cmd.Flags().GetBool("today")
+        t := table.New().
+            Border(lipgloss.NormalBorder()).
+            Headers("ID", "Date", "Time", "Description")
+
         for _, activity := range activities {
             if !today || activity.Date == time.Now().Format("2006-01-02") {
                 prefix := " "
@@ -66,9 +78,10 @@ var activityCmd = &cobra.Command{
                     elapsed := time.Since(started).Round(time.Second)
                     duration += elapsed
                 }
-                fmt.Printf("%s%d\t%s\t(%s)\n", prefix, activity.Id, activity.Description, duration.String())
+                t.Row(fmt.Sprintf("%d", activity.Id), activity.Date, fmt.Sprintf("%s%s", prefix, duration.String()), activity.Description)
             }
         }
+        fmt.Println(t)
     },
 }
 
